8bded52b3e39b23051f871377904b3730906a0c2
umayerCoding1 <umayerpro175@gmail.com>
2025-07-10 15:31:04 +0600

    Complete profile page

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -4,11 +4,13 @@ import { setUser } from "@/app/store/authSlice";
 import axios from "axios";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Button } from "./ui/button";
+import { Loader2 } from "lucide-react";
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const { user } = useSelector((state: any) => state.auth);
   const dispatch = useDispatch();
 
@@ -22,6 +24,8 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         }
       } catch (err) {
         console.log("User not logged in", err);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -32,6 +36,10 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
   if (!user && pathname !== "/login") {
     return (
+      <>
+      {isLoading ? <div className="flex items-center justify-center h-[400px]">
+        <Loader2 className="animate-spin" size={35} />
+      </div> :
       <div className="flex items-center justify-center flex-col gap-3 h-[400px]">
         <p> you are not logged in please login!</p>
 
@@ -39,6 +47,8 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
           <Link href="/login">Login</Link>
         </Button>
       </div>
+      }
+      </>
     );
   }
   return <> {children} </>;
--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,14 +1,28 @@
 'use client'
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Image from 'next/image';
 
+
+export const myImage = '/assets/my-image.jpg';
+export const myResume = '/assets/my-resume.pdf';
 const Profile = () => {
     const user  = useSelector((state:any) => state.auth.user);
+    console.log(user);
     
         
     return (
-        <div>
-            <h1>Profile</h1>
+        <div className='relative p-10'>
+           <Image src={myImage} alt='my-image' width={500}
+      height={500} className='w-80 h-80 rounded-full'/>
+
+      <iframe 
+       src={myResume}
+       width="20%"
+       title='my resume'
+       height="200px"
+       className="border-0"
+      />
         </div>
     );
 };
